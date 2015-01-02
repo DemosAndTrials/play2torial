6c734ba0db64581d6a77ec7db7920507df1d185c
Ros Haitovich <ros.haitovich@kerensen.com>
2015-01-02 11:39:12 +0200

    add Procfile for Heroku

--- test/FooTest.java
+++ test/FooTest.java
@@ -1,5 +1,18 @@
 /**
  * Created by Ros on 1/2/2015.
  */
+import org.junit.Test;
+import play.mvc.Result;
+
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
+
 public class FooTest {
+
+    @Test
+    public void testFooRoute() {
+        Result result = routeAndCall(fakeRequest(GET, "/foo"));
+        assertThat(result).isNotNull();
+    }
+
 }
